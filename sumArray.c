f92f7756ac014b1bafdb2c1de13155299278947c
prashantkmps <31593739+prashantkmps@users.noreply.github.com>
2018-01-08 22:01:43 +0530

    Create sumArray.c

---
+++ sumArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a[100],s = 0,n;
+
+     scanf("%d%d",&n,&k);
+
+    while(k >0)
+    {
+        sum=sum+a[k-1];
+        k--;
+        
+    }
+
+    printf("%d",sum);
+}
