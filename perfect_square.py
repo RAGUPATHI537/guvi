e4fa7aabfdaece2a15677b59d61fa3487c3ce80b
prashantkmps <31593739+prashantkmps@users.noreply.github.com>
2018-01-13 22:40:34 +0530

    Update perfect_square.py

--- perfect_square.py
+++ perfect_square.py
@@ -4,8 +4,8 @@ i,r =input().split()
 i=int(i)
 r=int(r)
 print(type(i))
-c=0
-for q in range(i,r+1):
-  if int(math.sqrt(q)) ==q**2:
-    count++
-print(count)
+count=0
+for q in range(i,r):
+  if int(math.sqrt(q))**2==q:
+      count=count+1
+print(count) 
