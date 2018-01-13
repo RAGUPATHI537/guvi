82c845c96429f5dce17379dbf6c95179daac27ae
prashantkmps <31593739+prashantkmps@users.noreply.github.com>
2018-01-13 22:09:02 +0530

    Create space.py

---
+++ space.py
@@ -0,0 +1,17 @@
+def removeSpaces(string):
+ 
+     count = 0
+ 
+    list = []
+ 
+     
+    for i in xrange(len(string)):
+        if string[i] != ' ':
+            list.append(string[i])
+ 
+    return toString(list)
+ 
+ def toString(List):
+    return ''.join(List)
+ a=input()
+ print removeSpaces(a)
